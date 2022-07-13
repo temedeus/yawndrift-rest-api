bfed75d8995f3d1e1063c74591a75ff8f4d8f437
Teemu Puurunen <11508907+temedeus@users.noreply.github.com>
2022-07-13 15:47:15 +0300

    MongoDB connection

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true, useUnifiedTopology: true,
+    }).then(() => {
+    console.log('Success connecting to database!');
+}).catch((err) => {
+    console.error('Error connecting to database!', err);
+});
 
 app.use(express.json);
 app.use(morgan("dev"));
