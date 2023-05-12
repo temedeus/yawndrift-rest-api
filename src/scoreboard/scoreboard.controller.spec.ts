5ab778e8e300d9e6fa4c971c2d5819211d477b2d
Teemu Puurunen <11508907+temedeus@users.noreply.github.com>
2023-05-12 17:13:15 +0300

    Test for fetching scoreboards

--- src/scoreboard/scoreboard.controller.spec.ts
+++ src/scoreboard/scoreboard.controller.spec.ts
@@ -18,5 +18,13 @@ describe('ScoreboardController', () => {
         expect(value).toBe(ScoreboardMock);
       } )
     });
+
+    it('should return list of Scoreboard mock', () => {
+      const scoreboardExpected = [ScoreboardMock, ScoreboardMock];
+      jest.spyOn(scoreboardService, 'scoreboards').mockImplementation((scoreboardWhereInput) => new Promise((resolve, reject) => { resolve(scoreboardExpected)}));
+      scoreboardController.getScoreboards('123').then(value => {
+        expect(value).toBe(scoreboardExpected);
+      } )
+    });
   });
 });
